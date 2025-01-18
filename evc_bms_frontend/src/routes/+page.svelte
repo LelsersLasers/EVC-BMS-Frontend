e72763c143b51760024385610cc871a5248f6c20
Millan Kumar <millankumar@gmail.com>
2025-01-17 20:47:32 -0500

    Setup for ip address of the BMS

--- evc_bms_frontend/src/routes/+page.svelte
+++ evc_bms_frontend/src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { onMount } from 'svelte';
+
+    let ipAddress = $state(null);
+    let ipAddressText = $state('No IP address set');
+
+    onMount(async () => {
+        const ls = localStorage.getItem('ipAddress');
+        if (ls) {
+            ipAddress = ls;
+        }
+    });
+
+    async function promptForIpAddress() {
+        let ip = prompt('Enter the IP address of the BMS');
+        if (ip) {
+            try {
+                if (!ip.startsWith('http://')) ip = `http://${ip}`;
+                if (ip.endsWith('/'))          ip = ip.slice(0, -1);
+
+                ipAddressText = 'Checking IP address...';
+
+                await fetch(`${ip}/connected`)
+                    .then(res => {
+                        if (!res.ok) throw new Error('Invalid IP address');
+                    });
+
+                ipAddressText = 'No IP address set';
+
+                ipAddress = ip;
+                localStorage.setItem('ipAddress', ip);
+            } catch (e) {
+                alert('Invalid IP address');
+                promptForIpAddress();
+            }
+
+            localStorage.setItem('ipAddress', ip);
+            ipAddress = ip;
+        } else {
+            alert('You must enter an IP address to continue');
+            promptForIpAddress();
+        }
+    }
+
+    $effect(() => {
+        if (!ipAddress) {
+            promptForIpAddress();
+        }
+    });
+</script>
+
+
+
+<h1>EVC BMS</h1>
+
+{#if ipAddress}
+    <p>IP Address: {ipAddress}</p>
+{:else}
+    <p>{ipAddressText}</p>
+{/if}
