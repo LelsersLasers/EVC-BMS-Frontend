1db947efb2484ed635a637b5b1600115060ba33c
Millan Kumar <millankumar@gmail.com>
2025-01-26 12:52:21 -0500

    Trying vercel?

--- evc_bms_frontend/svelte.config.js
+++ evc_bms_frontend/svelte.config.js
@@ -1,5 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,9 +9,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-		prerender: {
-			entries: ['*'], // Pre-render all routes
-		},
+		// prerender: {
+		// 	entries: ['*'], // Pre-render all routes
+		// },
 	}
 };
 
